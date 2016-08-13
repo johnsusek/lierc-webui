4a6acdcc9f1fb786aec43d86dc1f0313c92d8efe
John Susek <john@johnsolo.net>
2016-08-13 16:21:43 -0500

    Basic view model

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,44 @@ const store = {
         rootURL: 'http://lierc-webui.local:5004/',
         sessionID: '2zfbuYhqSURN2De31RJJ2c'
     },
-    ircEvents: []
+    ircEvents: [],
+    interface: {
+        activeChannelOrDirectMessage: '#example' // or a username
+    },
+    connections: [
+        {
+            isConnected: true,
+            server: 'irc.freenode.com',
+            port: '6667',
+            ssl: false,
+            nickname: 'Jane Doe',
+            username: 'janedoe',
+            password: 'secret',
+            autoJoinChannels: ['#example', '#example2']
+        }
+    ],
+    channels: [
+        {
+            name: '#example',
+            topic: 'Example topic.',
+            users: ['alice', 'bob', 'charlie'],
+            isJoined: true,
+            unreadCount: 0,
+            messages: [
+                { user: 'alice', message: 'content', timestamp: Date() }
+            ]
+        }
+    ],
+    directMessages: [
+        {
+            unreadCount: 0,
+            user: 'charlie',
+            messages: [
+                { user: 'charlie', message: 'message from charlie', timestamp: Date() },
+                { user: 'johnsusek', message: 'reply!', timestamp: Date() }
+            ]
+        }
+    ]
 }
 
 export default store
