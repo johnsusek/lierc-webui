b00ebc1dd27ce278d6ac0f455b8872b857503b47
John Susek <john@johnsolo.net>
2016-08-21 12:14:08 -0500

    Config reset

--- config/index.js
+++ config/index.js
@@ -21,13 +21,7 @@ module.exports = {
     port: 8080,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {
-        '/api': {
-            target: 'https://chat.prettybrd.com',
-            secure: true,
-            changeOrigin: true
-        }
-    },
+    proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
